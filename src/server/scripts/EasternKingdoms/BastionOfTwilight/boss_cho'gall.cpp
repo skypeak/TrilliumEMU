a176f2736bb8b9d950b15444d601085fd33a8c21
Spyder <serge@arkania.net>
2011-10-11 15:17:38 +0200

    Core/Scripts: Instance : BastionOfTwilight fixes

--- src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_cho'gall.cpp
+++ src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_cho'gall.cpp
@@ -18,3 +18,45 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ 
+#include"ScriptPCH.h"
+#include"bastion_of_twilight.h"
+
+class boss_chogall : public CreatureScript
+{
+public:
+    boss_chogall() : CreatureScript("boss_chogall") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new boss_chogallAI (creature);
+    }
+
+    struct boss_chogallAI : public ScriptedAI
+    {
+        boss_chogallAI(Creature* creature) : ScriptedAI(creature)
+        {
+            pInstance = creature->GetInstanceScript();
+        }
+
+        InstanceScript* pInstance;
+
+        void Reset() {}
+
+        void EnterCombat(Unit* /*pWho*/) {}
+
+        void UpdateAI(const uint32 Diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+void AddSC_boss_chogall()
+{
+    new boss_chogall();
+}
