d6f38f86ef884d0b60fb3c6a56e7ce7010aaff2a
Spyder <serge@arkania.net>
2011-10-12 14:36:34 +0200

    Some changes on AI Scripts

--- src/server/game/AI/CoreAI/GuardAI.cpp
+++ src/server/game/AI/CoreAI/GuardAI.cpp
@@ -62,7 +62,7 @@ void GuardAI::MoveInLineOfSight(Unit* unit)
 
     if (!me->getVictim() && me->IsValidAttackTarget(unit) &&
         (unit->IsHostileToPlayers() || me->IsHostileTo(unit)) &&
-        unit->isInAccessiblePlacefor (me))
+        unit->isInAccessiblePlaceFor (me))
     {
         float attackRadius = me->GetAttackDistance(unit);
         if (me->IsWithinDistInMap(unit, attackRadius))
