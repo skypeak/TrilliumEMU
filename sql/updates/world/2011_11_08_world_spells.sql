ba525714b2dc2ffeb77678659182e3cf59df3d15
impas <kakaskar@yandex.ru>
2011-11-10 12:40:54 +0100

    correct 2011_11_08_world_spells.sql

--- sql/updates/world/2011_11_08_world_spells.sql
+++ sql/updates/world/2011_11_08_world_spells.sql
@@ -1,2 +1,3 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=86719;
 INSERT INTO `spell_linked_spell` VALUES (86719,82739,0,"Flame Orb - Damage");
 UPDATE `creature_template` SET `ScriptName`='npc_flame_orb' WHERE `entry`=30702;
