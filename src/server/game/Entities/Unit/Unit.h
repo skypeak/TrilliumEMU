a692138af241d1c8d2d7a4a113bceb7af86d381d
Serge Winters <serge@arkania.net>
2011-11-12 18:03:17 -0500

    Some cleanup of add/remove to/from map. Author: megamage

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1365,7 +1365,8 @@ class Unit : public WorldObject
         void SetArmor(int32 val) { SetResistance(SPELL_SCHOOL_NORMAL, val); }
 
         uint32 GetResistance(SpellSchools school) const { return GetUInt32Value(UNIT_FIELD_RESISTANCES+school); }
-        void SetResistance(SpellSchools school, int32 val) { SetStatInt32Value(UNIT_FIELD_RESISTANCES+school, val); }
+        uint32 GetResistance(SpellSchoolMask mask) const;
+		void SetResistance(SpellSchools school, int32 val) { SetStatInt32Value(UNIT_FIELD_RESISTANCES+school, val); }
 
         uint32 GetHealth()    const { return GetUInt32Value(UNIT_FIELD_HEALTH); }
         uint32 GetMaxHealth() const { return GetUInt32Value(UNIT_FIELD_MAXHEALTH); }
