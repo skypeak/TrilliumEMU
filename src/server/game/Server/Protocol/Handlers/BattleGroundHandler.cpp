103e7bc220e3900ff1e9e77be3eaebe04be9882a
Intel <chemicstry@gmail.com>
2011-11-12 22:44:47 +0200

    Core/Handlers: Some research on BattleGround Join structure

--- src/server/game/Server/Protocol/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/BattleGroundHandler.cpp
@@ -75,17 +75,19 @@ void WorldSession::SendBattlegGroundList(uint64 guid, BattlegroundTypeId bgTypeI
 
 void WorldSession::HandleBattlemasterJoinOpcode(WorldPacket & recv_data)
 {
-    uint64 guid;
     uint32 bgTypeId_;
-    uint32 instanceId;
-    uint8 joinAsGroup;
+    uint8 joinFlags;
     bool isPremade = false;
     Group* grp = NULL;
 
-    recv_data >> guid;                                      // battlemaster guid
-    recv_data >> bgTypeId_;                                 // battleground type id (DBC id)
-    recv_data >> instanceId;                                // instance id, 0 if First Available selected
-    recv_data >> joinAsGroup;                               // join as group
+    // Packet size is 8 bytes, lots of unknown data
+    recv_data >> joinFlags;         // join flags
+    recv_data.read_skip<uint8>();   // unk
+    recv_data.read_skip<uint32>();  // BG id? somewhy is always 0
+    recv_data.read_skip<uint8>();   // unk
+    recv_data.read_skip<uint8>();   // unk
+    
+    bool joinAsGroup = (joinFlags & 0x01); // Make flags enum?
 
     if (!sBattlemasterListStore.LookupEntry(bgTypeId_))
     {
@@ -101,7 +103,7 @@ void WorldSession::HandleBattlemasterJoinOpcode(WorldPacket & recv_data)
 
     BattlegroundTypeId bgTypeId = BattlegroundTypeId(bgTypeId_);
 
-    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: Recvd CMSG_BATTLEMASTER_JOIN Message from (GUID: %u TypeId:%u)", GUID_LOPART(guid), GuidHigh2TypeId(GUID_HIPART(guid)));
+    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: Recvd CMSG_BATTLEMASTER_JOIN Message from");
 
     // can do this, since it's battleground, not arena
     BattlegroundQueueTypeId bgQueueTypeId = BattlegroundMgr::BGQueueTypeId(bgTypeId, 0);
@@ -111,13 +113,9 @@ void WorldSession::HandleBattlemasterJoinOpcode(WorldPacket & recv_data)
     if (_player->InBattleground())
         return;
 
-    // get bg instance or bg template if instance not found
-    Battleground* bg = NULL;
-    if (instanceId)
-        bg = sBattlegroundMgr->GetBattlegroundThroughClientInstance(instanceId, bgTypeId);
+    // As of 4.x BG instances are not selectable
+    Battleground* bg = sBattlegroundMgr->GetBattlegroundTemplate(bgTypeId);
 
-    if (!bg)
-        bg = sBattlegroundMgr->GetBattlegroundTemplate(bgTypeId);
     if (!bg)
         return;
 
