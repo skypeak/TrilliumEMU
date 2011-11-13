a2ca545a7e8829cb24966b3ef8253c5ae8172973
Venugh <Venugh@gmx.net>
2011-11-13 23:25:43 +0100

    Core/DataStores: Shapeshift fixes.

--- src/server/game/DataStores/DataStructure.h
+++ src/server/game/DataStores/DataStructure.h
@@ -1448,14 +1448,14 @@ struct QuestSortEntry
 
 struct QuestXPEntry
 {
-  uint32      id;
-  uint32      Exp[10];
+    uint32      id;
+    uint32      Exp[10];
 };
 
 struct QuestFactionRewEntry
 {
-  uint32      id;
-  int32       QuestRewFactionValue[10];
+    uint32      id;
+    int32       QuestRewFactionValue[10];
 };
 
 struct RandomPropertiesPointsEntry
@@ -1809,9 +1809,9 @@ struct SpellScalingEntry
 struct SpellShapeshiftEntry
 {
     uint32    Id;                                           // 0 - m_ID
-    uint32    Stances;                                      // 1 - m_shapeshiftMask
+    uint32    StancesNot;                                   // 1 - m_shapeshiftExclude
     // uint32 unk_320_2;                                    // 2 - 3.2.0
-    uint32    StancesNot;                                   // 3 - m_shapeshiftExclude
+    uint32    Stances;                                      // 3 - m_shapeshiftMask
     // uint32 unk_320_3;                                    // 4 - 3.2.0
     // uint32    StanceBarOrder;                            // 5 - m_stanceBarOrder not used
 };
