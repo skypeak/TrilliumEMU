ebdacb738e81bf1ece4c3309cb8e0c5fe4c71de7
Venugh <Venugh@gmx.net>
2011-11-26 12:37:14 +0100

    Tools/Extractor: Update to 4.2.2.

--- src/server/game/Maps/TransportMgr.cpp
+++ src/server/game/Maps/TransportMgr.cpp
@@ -335,7 +335,7 @@ Transport* TransportMgr::CreateTransport(uint32 entry, Map* map /*= NULL*/)
     }
 
     // use preset map for instances (need to know which instance)
-    trans->SetMap(map ? map : sMapMgr->CreateMap(mapId, trans));
+    //trans->SetMap(map ? map : sMapMgr->CreateMap(mapId));
     trans->SetZoneScript();
 
     // Get all spawns on Transport map
