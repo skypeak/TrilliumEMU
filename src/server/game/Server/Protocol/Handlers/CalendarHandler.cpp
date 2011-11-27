22fb3a01d0fa58849eb66854e8024899d9963722
Venugh <Venugh@gmx.net>
2011-11-27 15:49:45 +0100

    Core/Calendar: Fix more packets.

--- src/server/game/Server/Protocol/Handlers/CalendarHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/CalendarHandler.cpp
@@ -197,97 +197,97 @@ void WorldSession::HandleCalendarGuildFilter(WorldPacket &recv_data)
 
 void WorldSession::HandleCalendarArenaTeam(WorldPacket &recv_data)
 {
-//    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: CMSG_CALENDAR_ARENA_TEAM");
-//    recv_data.hexlike();
-//	uint32 unk1;
-//    recv_data >> unk1;
+    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: CMSG_CALENDAR_ARENA_TEAM");
+    recv_data.hexlike();
+    uint32 unk1;
+    recv_data >> unk1;
 }
 
 void WorldSession::HandleCalendarAddEvent(WorldPacket& recv_data)
 {
-//    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: CMSG_CALENDAR_ADD_EVENT");
-//	uint64 eventID = sCalendarMgr->GetCurrentEventID();
-//	std::string eventName = "";
-//    std::string eventDescription = "";
-//	int8 type;
-//    int8 unk1;
-//    uint32 maxInvites ;
-//    uint32 dungeonId = 0;
-//    uint32 time;
-//    uint32 unkTime;
-//    uint32 flags;
-//	uint32 inviteCount;
-//	uint64 targetGuid;
-//	uint8 status;
-//	uint8 rank;
-//
-//    recv_data >> eventName;
-//    recv_data >> eventDescription;
-//    recv_data >> type;
-//    recv_data >> unk1;
-//    recv_data >> maxInvites;
-//    recv_data >> dungeonId;
-//    recv_data >> time;
-//    recv_data >> unkTime;
-//    recv_data >> flags;
-//	recv_data >> inviteCount;
-//
-//	Calendar_Event *f_event = sCalendarMgr->GetEvent(eventID);
-//	if (!f_event)
-//	{
-//		Calendar_Event *c_event = new Calendar_Event;
-//		eventID =  sCalendarMgr->GenerateNextEventID();
-//		c_event->eventID = eventID;
-//		c_event->creator_guid = GetPlayer()->GetGUIDLow();
-//		c_event->name = eventName;
-//		c_event->description = eventDescription;
-//		c_event->type = type;
-//		c_event->unk1 = unk1;
-//		c_event->dungeonID = dungeonId;
-//		c_event->time =time;
-//		c_event->unkTime = unkTime;
-//		c_event->flags = flags;
-//		c_event->guildID = 0;
-//
-//		/*sCalendarMgr->AddOrUpdateCalendarEvent(*c_event, true);*/
-//	}
-//    if (((flags >> 6) & 1))
-//        return;
-//
-//    if (!inviteCount)
-//        return;
-//
-//	recv_data.readPackGUID(targetGuid);
-//	recv_data >> status;
-//    recv_data >> rank;
-//	//Find From calendar_event_invite table = target Guild and eventid, update oder create new invite
-//	Calendar_Invite *t_invite = sCalendarMgr->GetInviteFromTeargetGuid(targetGuid,eventID);
-//	if (!t_invite)
-//	{
-//		uint64 newInviteID = sCalendarMgr->GenerateNextInviteID();
-//		Calendar_Invite *c_invite = new Calendar_Invite;
-//		c_invite->inviteID = newInviteID;
-//		c_invite->eventID = eventID;
-//		c_invite->rank = rank;
-//		c_invite->status = status;
-//		c_invite->unk1 = 0;
-//		c_invite->unk2 = 0;
-//		c_invite->creator_guid = GetPlayer()->GetGUIDLow();
-//		c_invite->time =time;
-//		c_invite->target_guid=targetGuid;
-//		c_invite->text="Nothing";
-//
-//		sCalendarMgr->AddOrUpdateCalendarInvite(*c_invite, true);
-//	}
-//    else
-//	{
-//		/*t_invite->status = status;
-//		t_invite->rank = rank;
-//		sCalendarMgr->AddOrUpdateCalendarInvite(*t_invite,false);
-//		*/
-//	}
-//
-//	SendCalendarEvent(eventID, true);
+    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: CMSG_CALENDAR_ADD_EVENT");
+    uint64 eventID = sCalendarMgr->GetCurrentEventID();
+    std::string eventName = "";
+    std::string eventDescription = "";
+    int8 type;
+    int8 unk1;
+    uint32 maxInvites ;
+    uint32 dungeonId = 0;
+    uint32 time;
+    uint32 unkTime;
+    uint32 flags;
+    uint32 inviteCount;
+    uint64 targetGuid;
+    uint8 status;
+    uint8 rank;
+
+    recv_data >> eventName;
+    recv_data >> eventDescription;
+    recv_data >> type;
+    recv_data >> unk1;
+    recv_data >> maxInvites;
+    recv_data >> dungeonId;
+    recv_data >> time;
+    recv_data >> unkTime;
+    recv_data >> flags;
+    recv_data >> inviteCount;
+
+    Calendar_Event *f_event = sCalendarMgr->GetEvent(eventID);
+    if (!f_event)
+    {
+        Calendar_Event *c_event = new Calendar_Event;
+        eventID =  sCalendarMgr->GenerateNextEventID();
+        c_event->eventID = eventID;
+        c_event->creator_guid = GetPlayer()->GetGUIDLow();
+        c_event->name = eventName;
+        c_event->description = eventDescription;
+        c_event->type = type;
+        c_event->unk1 = unk1;
+        c_event->dungeonID = dungeonId;
+        c_event->time =time;
+        c_event->unkTime = unkTime;
+        c_event->flags = flags;
+        c_event->guildID = 0;
+
+        sCalendarMgr->AddOrUpdateCalendarEvent(*c_event, true);
+    }
+    if (((flags >> 6) & 1))
+        return;
+
+    if (!inviteCount)
+        return;
+
+    recv_data.readPackGUID(targetGuid);
+    recv_data >> status;
+    recv_data >> rank;
+    // Find From calendar_event_invite table = target Guild and eventid, update oder create new invite
+    Calendar_Invite *t_invite = sCalendarMgr->GetInviteFromTeargetGuid(targetGuid,eventID);
+    if (!t_invite)
+    {
+        uint64 newInviteID = sCalendarMgr->GenerateNextInviteID();
+        Calendar_Invite *c_invite = new Calendar_Invite;
+        c_invite->inviteID = newInviteID;
+        c_invite->eventID = eventID;
+        c_invite->rank = rank;
+        c_invite->status = status;
+        c_invite->unk1 = 0;
+        c_invite->unk2 = 0;
+        c_invite->creator_guid = GetPlayer()->GetGUIDLow();
+        c_invite->time =time;
+        c_invite->target_guid=targetGuid;
+        c_invite->text="Nothing";
+
+        sCalendarMgr->AddOrUpdateCalendarInvite(*c_invite, true);
+    }
+    else
+    {
+	    /*t_invite->status = status;
+	    t_invite->rank = rank;
+	    sCalendarMgr->AddOrUpdateCalendarInvite(*t_invite,false);
+	    */
+    }
+
+    SendCalendarEvent(eventID, true);
 }
 
 void WorldSession::HandleCalendarUpdateEvent(WorldPacket &recv_data)
