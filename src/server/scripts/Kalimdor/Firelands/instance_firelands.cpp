a5d3284ed1e1570493021a7e2937891decdedd46
Naios <naios-dev@live.de>
2011-12-07 01:20:05 +0100

    Scripts/FL & Shannox compile errors fixed Shannox hurling speer timer fixed (i forgot { } ^^) Now Shannox is using his waypoints in the 25N, 10H and 25H raid version Yells added and fixed (the intro is working now!) Some Code cleanup and a more...
    Signed-off-by: Naios <naios-dev@live.de>

--- src/server/scripts/Kalimdor/Firelands/instance_firelands.cpp
+++ src/server/scripts/Kalimdor/Firelands/instance_firelands.cpp
@@ -1,25 +1,25 @@
 /*
- * Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.org/>
- *
- * Copyright (C) 2008 - 2011 TrinityCore <http://www.trinitycore.org/>
- *
- * Copyright (C) 2011 TrilliumEMU <http://www.arkania.net/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
- 
- /* Script complete: 1% */
+* Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.org/>
+*
+* Copyright (C) 2008 - 2011 TrinityCore <http://www.trinitycore.org/>
+*
+* Copyright (C) 2011 TrilliumEMU <http://www.arkania.net/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Script complete: 2% */
 
 #include "ObjectMgr.h"
 #include "ScriptMgr.h"
@@ -158,9 +158,9 @@ public:
 		{
 			switch (gameObject->GetEntry())
 			{
-				case GOB_DOOR_BETHILAC:
-					uiBethilacDoor = gameObject->GetGUID();
-					HandleGameObject(uiBethilacDoor,true);
+			case GOB_DOOR_BETHILAC:
+				uiBethilacDoor = gameObject->GetGUID();
+				HandleGameObject(uiBethilacDoor,true);
 				break;
 			}
 		}
@@ -173,7 +173,7 @@ public:
 		{
 			switch (creature->GetEntry())
 			{
-			case NPC_BETHTILAC:
+			case BOSS_BETHTILAC:
 				break;
 			case BOSS_RHYOLITH:
 			case BOSS_ALYSRAZAR:
@@ -197,17 +197,17 @@ public:
 				HandleGameObject(uiBethilacDoor, state != IN_PROGRESS);			
 				break;
 			case DATA_RHYOLITH:
-			break;
+				break;
 			case DATA_ALYSRAZAR:
-			break;
+				break;
 			case DATA_SHANNOX:
-			break;
+				break;
 			case DATA_BALOROC:
-			break;
+				break;
 			case DATA_MAJORDOMUS:
-			break;
+				break;
 			case DATA_RAGNAROS:
-			break;
+				break;
 
 			}
 
@@ -309,9 +309,9 @@ public:
 		}
 
 		void Update(uint32 diff)
-            {
-                
-            }
+		{
+
+		}
 
 	};
 
